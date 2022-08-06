938b77f2006d753fea0bcb4131f8fee4d64f41c7
Vadim <hellovados@gmail.com>
2022-08-06 18:33:16 +0300

    Revert "space"
    This reverts commit 5834c0d658b92603d4706b2573fed4127a81a670.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,6 @@
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-  DocumentInitialProps,
-} from "../node_modules/next/document";
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from "../node_modules/next/document";
 
+<<<<<<< HEAD
 class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext
@@ -18,15 +12,29 @@ class MyDocument extends Document {
     console.log("hi");
     return (
       <Html lang="ru">
+=======
+
+ class MyDocument extends Document {
+
+static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+  const initialProps = await Document.getInitialProps(ctx);
+  return {...initialProps}
+}
+
+
+render(): JSX.Element {
+  return (
+    <Html lang="ru">
+>>>>>>> parent of 5834c0d... space
         <Head>
           <body>
-            <Main />
-            <NextScript />
+            <Main/>
+            <NextScript/>
           </body>
         </Head>
-      </Html>
-    );
-  }
+    </Html>
+  )
+}
 }
 
 export default MyDocument;
