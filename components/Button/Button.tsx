aa71149ab0c7c8fc51cf6a04dd92eed3ae69fd4b
Vadim <hellovados@gmail.com>
2022-08-06 18:12:31 +0300

    add document button and init project

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,15 +2,31 @@ import styles from "./Button.module.css";
 import { ButtonProps } from "./Button.props";
 import classNames from "../../node_modules/classnames/index";
 
-export const Button = ({ children, appearance }: ButtonProps): JSX.Element => {
+export const Button = ({
+  children,
+  appearance,
+  className,
+  arrow = "none",
+  ...props
+}: ButtonProps): JSX.Element => {
   return (
     <button
-      className={classNames(styles.button, {
+      className={classNames(styles.button, className, {
         [styles.primary]: appearance == "primary",
         [styles.ghost]: appearance == "ghost",
       })}
+      {...props}
     >
       {children}
+      {arrow !== "none" && (
+        <span
+          className={classNames(styles.arrow, {
+            [styles.down]: arrow == "down",
+          })}
+        >
+         
+        </span>
+      )}
     </button>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,9 @@ export default function Home() {
   return (
     <>
       <Htag tag="h1">Текст</Htag>
-      <Button appearance="primary">Hello</Button>
+      <Button appearance="primary" arrow="right">
+        Hello
+      </Button>
       <Button appearance="ghost">Hello</Button>
     </>
   );
