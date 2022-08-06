f98ee3b4ca30cd4d0f0bb33cdaca8c4c3f406b98
Vadim <hellovados@gmail.com>
2022-08-06 18:36:03 +0300

    dddd

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,12 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from "../node_modules/next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "../node_modules/next/document";
 
-<<<<<<< HEAD
 class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext
@@ -9,31 +15,18 @@ class MyDocument extends Document {
     return { ...initialProps };
   }
   render(): JSX.Element {
+    console.log("hi");
     return (
       <Html lang="ru">
-=======
-
- class MyDocument extends Document {
-
-static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
-  const initialProps = await Document.getInitialProps(ctx);
-  return {...initialProps}
-}
-
-
-render(): JSX.Element {
-  return (
-    <Html lang="ru">
->>>>>>> parent of 5834c0d... space
         <Head>
           <body>
-            <Main/>
-            <NextScript/>
+            <Main />
+            <NextScript />
           </body>
         </Head>
-    </Html>
-  )
-}
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
