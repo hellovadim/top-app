5834c0d658b92603d4706b2573fed4127a81a670
Vadim <hellovados@gmail.com>
2022-08-06 18:14:13 +0300

    space

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,26 +1,32 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from "../node_modules/next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "../node_modules/next/document";
 
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
 
- class MyDocument extends Document {
-
-static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
-  const initialProps = await Document.getInitialProps(ctx);
-  return {...initialProps}
-}
-
-
-render(): JSX.Element {
-  return (
-    <Html lang="ru">
+  render(): JSX.Element {
+    return (
+      <Html lang="ru">
         <Head>
           <body>
-            <Main/>
-            <NextScript/>
+            <Main />
+            <NextScript />
           </body>
         </Head>
-    </Html>
-  )
-}
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
